6847952e72478654a6a2bfcfa49f4e60aa2680cd
ElenaLj <elenaljubijankic@gmail.com>
2022-02-04 17:12:37 +0100

    insert header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header class="header">
+    <img src="{{asset("images/movie-database-logo.png")}}" alt="The Movie Database (TMDB)" class="header__logo">
+
+    <nav>
+        <ul class="header__items">
+            <li class="header__item">
+                <a href="#">Film</a>
+            </li>
+            <li class="header__item">
+                <a href="#">Serie TV</a>
+            </li>
+            <li class="header__item">
+                <a href="#">Persone</a>
+            </li>
+            <li class="header__item">
+                <a href="#">More</a>
+            </li>
+            <li class="header__item">
+                <a href="#">Accedi</a>
+            </li>
+            <li class="header__item">
+                <a href="#">Registrati</a>
+            </li>
+        </ul>
+    </nav>
+</header>
